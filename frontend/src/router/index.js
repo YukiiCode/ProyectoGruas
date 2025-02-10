934d3d692481a10a42e2de152b01e76396162904
Yuki <119506189+YukiiCode@users.noreply.github.com>
2025-02-10 10:44:56 +0100

    Actualizar dependencias y agregar vista de retiradas en el enrutador

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Login from '@/views/Login.vue';
+import RetiradasView from '@/views/RetiradasView.vue';
 import VehiculosList from '@/views/VehiculosList.vue';
 import RetiradasList from '@/views/RetiradasList.vue';
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
+  { path: '/retiradas/', component: RetiradasView },
   { path: '/vehiculos', component: VehiculosList },
   { path: '/retiradas', component: RetiradasList },
 ];
